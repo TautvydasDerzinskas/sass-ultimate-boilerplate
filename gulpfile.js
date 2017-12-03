bb2a20aa6b16f579864aaa7a4f6fbf6ba976fe25
SlimDogs <SlimDogs@gmail.com>
2017-12-03 11:23:06 +0000

    feat(Extended docs): Autogenerating features to the README.md
    Added logic which grabs comments from *.scss files and generates a README.md using these comments

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')
 const sassLint = require('gulp-sass-lint')
+const commentsToMd = require('gulp-comments-to-md')
+const gulpConcat = require('gulp-concat')
 
-gulp.task('[SASS] Compile', function () {
+gulp.task('[SASS] Compile', () => {
   return gulp.src('./src/sass-boilerplate.scss')
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
     .pipe(gulp.dest('./dist'))
 })
 
-gulp.task('[SASS] Lint', function () {
+gulp.task('[SASS] Lint', () => {
   return gulp.src('dist/**/*.scss')
     .pipe(sassLint({
       configFile: '.sass-lint.yml'
@@ -16,5 +18,22 @@ gulp.task('[SASS] Lint', function () {
     .pipe(sassLint.format())
     .pipe(sassLint.failOnError())
 })
+
+gulp.task('[Readme] Generate features', () => {
+  return gulp.src('src/**/*.scss')
+    .pipe(commentsToMd('features.md'))
+    .pipe(gulp.dest('docs/'))
+})
+
+gulp.task('[Readme] Generate README.md', ['[Readme] Generate features'], () => {
+  return gulp.src([
+    './docs/readme.md',
+    './docs/features.md'
+  ])
+  .pipe(gulpConcat('README.md'))
+  .pipe(gulp.dest('./'))
+})
+
+gulp.task('docs', ['[Readme] Generate README.md'])
 
 gulp.task('default', ['[SASS] Compile', '[SASS] Lint'])
